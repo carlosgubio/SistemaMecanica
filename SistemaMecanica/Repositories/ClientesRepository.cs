60ff63614ff18caaee5c914e1d220bd24be73f03
carlos gubio santana moreira <carlosgubio@gmail.com>
2022-08-15 22:23:24 -0300

    .

--- SistemaMecanica/Repositories/ClientesRepository.cs
+++ SistemaMecanica/Repositories/ClientesRepository.cs
@@ -12,7 +12,7 @@ namespace SistemaMecanica.Repositories
 {
     public class ClientesRepository
     {
-        private readonly string _connection = @"Data Source=ITELABD02\SQLEXPRESS;Initial Catalog=SistemaMecanica;Integrated Security=True;";
+        private readonly string _connection = @"Data Source=Gubio\SQLEXPRESS;Initial Catalog=SistemaMecanica;Integrated Security=True;";
         public bool SalvarCliente(CadastrarClienteViewModel salvarPessoaViewModel)
         {
             //int IdClienteCriada = -1;
