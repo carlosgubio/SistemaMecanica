2c4d1e8aa933944058cebd67d834151abada466b
Carlos Gubio <carlosgubio@gmail.com>
2022-09-30 11:28:21 -0300

    .

--- SistemaMecanica/ViewModelsCadastrar/CadastrarClienteViewModel.cs
+++ SistemaMecanica/ViewModelsCadastrar/CadastrarClienteViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaMecanica.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,5 +13,6 @@ namespace SistemaMecanica.ViewModels
         public string CpfCliente { get; set; }
         public string TelefoneCliente { get; set; }
         public string EnderecoCliente { get; set; }
+        public Veiculos veiculos { get; set; }
     }
 }
